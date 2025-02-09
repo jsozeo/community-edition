26d558f3896633c2bbbe44eb183351a18cf4a677
Jean-Sébastien Mancebo <93733241+jsozeo@users.noreply.github.com>
2025-02-09 09:41:00 +0100

    Add files via upload

---
+++ config/dev.exs
@@ -0,0 +1,31 @@
+import Config
+
+config :plausible, PlausibleWeb.Endpoint,
+  server: true,
+  debug_errors: true,
+  code_reloader: true,
+  check_origin: false,
+  watchers: [
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]},
+    storybook_tailwind: {Tailwind, :install_and_run, [:storybook, ~w(--watch)]},
+    npm: ["--prefix", "assets", "run", "typecheck", "--", "--watch", "--preserveWatchOutput"],
+    npm: [
+      "run",
+      "deploy",
+      cd: Path.expand("../tracker", __DIR__)
+    ]
+  ],
+  live_reload: [
+    dirs: [
+      "extra"
+    ],
+    patterns: [
+      ~r{priv/static/.*(js|css|png|jpeg|jpg|gif|svg)$},
+      ~r"lib/plausible_web/(controllers|live|components|templates|views|plugs)/.*(ex|heex)$",
+      ~r"storybook/.*(exs)$"
+    ]
+  ]
+
+config :phoenix, :stacktrace_depth, 20
+config :phoenix, :plug_init_mode, :runtime
---
+++ config/test.exs
@@ -0,0 +1,50 @@
+import Config
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :plausible, PlausibleWeb.Endpoint, server: false
+
+config :bcrypt_elixir, :log_rounds, 4
+
+config :plausible, Plausible.Repo,
+  pool: Ecto.Adapters.SQL.Sandbox,
+  pool_size: System.schedulers_online() * 2
+
+config :plausible, Plausible.ClickhouseRepo,
+  loggers: [Ecto.LogEntry],
+  pool_size: 15
+
+config :plausible, Plausible.Mailer, adapter: Bamboo.TestAdapter
+
+config :plausible,
+  paddle_api: Plausible.PaddleApi.Mock,
+  google_api: Plausible.Google.API.Mock
+
+config :bamboo, :refute_timeout, 10
+
+config :plausible,
+  session_timeout: 0,
+  http_impl: Plausible.HTTPClient.Mock
+
+config :plausible, Plausible.Cache, enabled: false
+
+config :ex_money, api_module: Plausible.ExchangeRateMock
+
+config :plausible, Plausible.Ingestion.Counters, enabled: false
+
+config :plausible, Oban, testing: :manual
+
+config :plausible, Plausible.Verification.Checks.FetchBody,
+  req_opts: [
+    plug: {Req.Test, Plausible.Verification.Checks.FetchBody}
+  ]
+
+config :plausible, Plausible.Verification.Checks.Installation,
+  req_opts: [
+    plug: {Req.Test, Plausible.Verification.Checks.Installation}
+  ]
+
+config :plausible, Plausible.HelpScout,
+  req_opts: [
+    plug: {Req.Test, Plausible.HelpScout}
+  ]
